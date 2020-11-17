4f7aec4108646e947fdb79eaf0ac7ed6e16ac566
ivan <ivanes285@gmail.com>
2020-11-16 19:17:16 -0500

    Third commit

--- js/game.js
+++ js/game.js
@@ -1,12 +1,30 @@
 const WIDTH=500;
 const HEIGTH=500;
 
+//puntos ejee Y y X en el plano y le damos randomico 
 let target = {
     x: getRandomNumbers(WIDTH),
     y: getRandomNumbers(HEIGTH)
 } 
+console.log(target)
 
-console.log(target);
+//Referenciamos a la imagen con el id
+//El $ nos ayuda hacer referencia que es un elemento hmtl nada mas 
+
+let $map = document.getElementById('map');
+let $distance = document.getElementById('distance');
+let clicks =0;
+
+$map.addEventListener('click',(event)=>{
+    clicks++;
+    let distance = getDistance(event,target)
+    $distance.innerHTML= `<h1> ${getDistanceHint(distance)} </h1>` 
+    if(distance<30){
+     alert(`You Have Found the Treasure 😄 in ${clicks} clicks `)
+     location.reload() 
+    }
+   
+})
 
 
 
--- js/helpers.js
+++ js/helpers.js
@@ -4,26 +4,33 @@ let getRandomNumbers = (size) => {
   return Math.floor(Math.random() * size);
 };
 
+//obtiene la distancia con dos parametros el primero es el punto del click  y el target que
+//se obtiene randomicamente 
+
 let getDistance = (eventclick, target) => {
   let diffx = eventclick.offsetX - target.x;
   let diffy = eventclick.offsetY - target.y;
   return Math.sqrt(Math.pow(diffx, 2) + Math.pow(diffy, 2));
 };
 
+
+
 let getDistanceHint = (distance) => {
-  if (distance < 20) {
-    return "Boiling Hot";
-  } else if (distance < 40) {
-    return "Really Hot";
-  } else if (distance < 80) {
-    return "Hot";
-  } else if (distance < 120) {
-    return "Warm";
-  } else if (distance < 280) {
-    return "Cold";
+  if (distance < 40) {
+    return "Boiling Hot(Hirviendo)";
+  } else if (distance < 65) {
+    return "Really Hot (Muy Caliente)";
+  } else if (distance < 105) {
+    return "Hot (Caliente)";
+  } else if (distance < 165) {
+    return "Warm (Calido)";
+  } else if (distance < 225) {
+    return "Cold (frío)";
   } else if (distance < 360) {
-    return "Really Cold";
+    return "Really Cold (Muy Frío)";
+  }else if (distance < 420) {
+      return "Frozen (Helado)";
   } else {
-    return "Freezing";
+    return "Freezing (Congelado)";
   }
 };
